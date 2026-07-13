a666cda8dd0efb9c63a514c8289b5572cb4d012f
Etienne Callies <etienne.callies@ouihelp.fr>
2026-07-13 15:07:43 +0200

    upgrade chrome version in user-agent

--- crawling/workflows/download/download_content.py
+++ crawling/workflows/download/download_content.py
@@ -25,7 +25,7 @@ MAX_SIZE = 1_000_000
 def get_headers():
     return {
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) '
-                      'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36'
+                      'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36'
     }
 
 
