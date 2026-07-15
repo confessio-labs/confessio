92e63cef591bf5b0381b36de640e900a89aaa2d2
Etienne Callies <etienne.callies@ouihelp.fr>
2026-07-15 15:30:24 +0200

    sort periods + add it at event-level

--- front/front_api.py
+++ front/front_api.py
@@ -41,6 +41,7 @@ class EventOut(Schema):
     end: datetime | None
     schedules_indices: list[int]
     source_has_been_moderated: bool
+    periods: list[PeriodEnum]
 
     @classmethod
     def from_index_event(cls, index_event: IndexEvent) -> 'EventOut':
@@ -53,6 +54,7 @@ class EventOut(Schema):
             end=end,
             schedules_indices=index_event.schedules_indices,
             source_has_been_moderated=index_event.has_been_moderated,
+            periods=index_event.periods,
         )
 
 
@@ -199,7 +201,7 @@ class ChurchDetails(ChurchOut):
     website: WebsiteOut
     schedules: list[ScheduleOut]
     parsings: list[ParsingOut]
-    periods: list[PeriodEnum]
+    periods: list[PeriodEnum]  # TODO legacy
 
     @classmethod
     def from_church_and_schedules(cls, church: Church, index_events: list[IndexEvent],
@@ -208,15 +210,19 @@ class ChurchDetails(ChurchOut):
                                   parsings: list[ParsingOut],
                                   ) -> 'ChurchDetails':
         base = ChurchOut.from_church_and_events(church, index_events)
-        periods = list(set(period for index_event in index_events
-                           for period in index_event.periods))
+        # TODO legacy
+        periods_seen = set()
+        periods = [
+            period for event in sorted(index_events) for period in event.periods
+            if period not in periods_seen and not periods_seen.add(period)
+        ]
 
         return cls(
             **base.dict(),
             website=website,
             schedules=schedules,
             parsings=parsings,
-            periods=periods,
+            periods=periods,   # TODO legacy
         )
 
 
